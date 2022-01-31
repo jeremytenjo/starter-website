152dd472e7a6d2b9652e1150662e7e9a58c0ae65
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-01-31 11:20:54 -0500

    cop

--- devtools/utils/node/copyToClipboard.ts
+++ devtools/utils/node/copyToClipboard.ts
@@ -5,6 +5,8 @@ import log, { chalk } from './log.js'
 type Props = {
   text: string
 }
+
+// https://github.com/sindresorhus/clipboardy
 export default function copyToClipboard({ text }: Props) {
   clipboard.writeSync(text)
   log(`${chalk.cyan(text)} copied to clipboard`)

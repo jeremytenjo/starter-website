9f43287d222644246ffeed8e2a49791cd72e2f42
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-01-31 11:21:36 -0500

    clean

--- src/lib/components/icons/IconHom.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-import { createSvgIcon } from '@mui/material'
-
-export default createSvgIcon(<svg>asdfasd</svg>, 'IconHom')
--- src/lib/components/icons/asdf.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-import { createSvgIcon } from '@mui/material'
-
-export default createSvgIcon(<svg>asdf</svg>, 'asdf')

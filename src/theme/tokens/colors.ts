0aeca52be1208f935ef04297b862d5dd5ac495b1
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-02-01 15:30:32 -0500

    colors

--- src/theme/tokens/colors.ts
+++ src/theme/tokens/colors.ts
@@ -1,16 +1,13 @@
 const colors: any = {
   primary: {
     main: '#5D59FF',
-    light: '#EAEAFF',
   },
   gradient: {
     primary: 'linear-gradient(83.6deg, #5D59FF 8.71%, #9076FF 98.37%)',
-    red: 'linear-gradient(83.6deg, #FF5959 8.71%, #FF7676 98.37%)',
   },
-  grey: { main: '#8c8c8c', light: '#DEDEDE' },
+  grey: { main: '#8c8c8c' },
   black: {
     main: '#1C1929',
-    lighter: '#373543',
   },
   white: {
     main: '#fff',

c2e3d8fdbf7eabda35f5674984bba4b7bea41409
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-03-02 09:33:26 -0500

    intru

--- src/lib/components/analytics/GoogleAnalytics/googleAnalytics.config.ts
+++ src/lib/components/analytics/GoogleAnalytics/googleAnalytics.config.ts
@@ -3,6 +3,7 @@ type GoogleAnalyticsConfigSchema = {
 }
 
 const googleAnalyticsConfig: GoogleAnalyticsConfigSchema = {
+  // https://www.optimizesmart.com/understanding-measurement-id-in-ga4-google-analytics-4/#1-how-to-find-google-analytics-4-measurement-id-
   measurementId: undefined,
 }
 

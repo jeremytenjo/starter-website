06c2f3122d69b155e73b91e267001137e711820d
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-03-16 21:18:14 -0400

    data

--- devtools/codeGenerator/templates/data.cjs
+++ devtools/codeGenerator/templates/data.cjs
@@ -20,11 +20,43 @@ const files = [
       const upperName = helpers.changeCase.capitalCase(name).split(' ').join('')
 
       return `
-      import ${upperName}Schema from './${name}.schema'
+      import type ${upperName}Schema from './${name}.schema'
 
       const ${upperName}Stubs: ${upperName}Schema[] = []
       
       export default ${upperName}Stubs
+
+      `
+    },
+  },
+  {
+    path: ({ name, helpers }) => {
+      const upperName = helpers.changeCase.capitalCase(name).split(' ').join('')
+      return `${name}.api/get${upperName}.tsx`
+    },
+    template: ({ name, helpers }) => {
+      const upperName = helpers.changeCase.capitalCase(name).split(' ').join('')
+
+      return `
+      import ${name}Stubs from '../${name}.stubs'
+      import type ${upperName}Schema from '../${name}.schema'
+      
+      export default async function get${upperName}() {
+        const isDev = process.env.NODE_ENV === 'development'
+        const ${name}: ${upperName}Schema[] = isDev
+          ? ${name}Stubs
+          : await get${upperName}FromApi()
+      
+        return ${name}
+      }
+      
+      const get${upperName}FromApi = () => {
+        const ${name}: ${upperName}Schema[] = []
+        // TODO get ${name} from api
+      
+        return ${name}
+      }
+      
 
       `
     },

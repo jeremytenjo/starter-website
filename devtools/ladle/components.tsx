265f366fe584d5b6a200ab5353b741fe90ac80fb
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-05-05 13:59:05 -0400

    c

--- devtools/ladle/components.tsx
+++ devtools/ladle/components.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import type { GlobalProvider } from '@ladle/react'
 
 import Theme from '../../src/theme/theme'
 import updateFavIcon from '../utils/images/updateFavIcon/updateFavIcon'
@@ -8,6 +7,6 @@ import './ladle.css'
 
 updateFavIcon({ iconPath: 'images/logo/logo-storybook.svg' })
 
-export const Provider: GlobalProvider = ({ children }) => {
+export const Provider = ({ children }) => {
   return <Theme>{children}</Theme>
 }

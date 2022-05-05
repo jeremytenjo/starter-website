7ddc9d72893c1afdcf41d5d9043caa633bdf8730
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-05-04 23:29:56 -0400

    script

--- scripts/dev/dev.ts
+++ scripts/dev/dev.ts
@@ -2,17 +2,28 @@ import chalk from 'chalk'
 
 import getIpAdress from '../../devtools/utils/node/getIpAdress.js'
 import shell from '../../devtools/utils/node/shell.js'
-import setType from '../../devtools/utils/pkgJson/setType/setType.cjs'
+import getCommandLineArgs from '../../devtools/utils/node/getCommandLineArgs.js'
 
 export default function dev() {
   console.clear()
-
   const ipAdress = getIpAdress()
-
   console.log(`${chalk.green('network')} - http://${ipAdress}:3000`)
 
-  // TODO remove setType when https://github.com/vercel/next.js/pull/33637 is merged
-  setType({ type: 'commonjs' })
+  // args
+  const scriptArgs = getCommandLineArgs([
+    { name: 'dataSource', type: String },
+    {
+      name: 'onlyApp',
+      type: Boolean,
+    },
+  ])
+  const dataSource = scriptArgs.dataSource || 'dev'
 
-  shell('node_modules/.bin/next dev')
+  const commands = [`DATA_SOURCE=${dataSource} node_modules/.bin/next dev`]
+
+  if (!scriptArgs.onlyApp) {
+    commands.push('npm run ladle:dev')
+  }
+
+  shell(commands)
 }

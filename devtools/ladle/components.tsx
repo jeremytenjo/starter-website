aa5f7c867d9d5f466d07ad7ba4bd0afe5efb225d
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-05-09 22:40:38 -0400

    added ClickToComponent to ladle

--- devtools/ladle/components.tsx
+++ devtools/ladle/components.tsx
@@ -2,11 +2,17 @@ import React from 'react'
 
 import Theme from '../../src/theme/theme'
 import updateFavIcon from '../utils/images/updateFavIcon/updateFavIcon'
+import ClickToComponent from '../../src/lib/components/ClickToComponent/ClickToComponent'
 
 import './ladle.css'
 
 updateFavIcon({ iconPath: 'images/logo/logo-storybook.svg' })
 
 export const Provider = ({ children }) => {
-  return <Theme>{children}</Theme>
+  return (
+    <>
+      <ClickToComponent />
+      <Theme>{children}</Theme>
+    </>
+  )
 }

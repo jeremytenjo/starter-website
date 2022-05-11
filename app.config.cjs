58c54b8f4e97f7ec07c0dd53a92bd2ef8acb56bb
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-05-11 18:23:40 -0400

    app config

---
+++ app.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  siteInfo: {
+    name: 'Website_Starter',
+    domain: 'https://starter-website.vercel.app/',
+    description: 'Website_StarterDesc',
+    title: 'Website_Starter',
+  },
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import Head from 'next/head'
 
+import appconfig from '../../app.config.cjs'
 import HomePageContent from '../content/HomePage/HomePage'
 
 export default function HomePage() {
   return (
     <>
       <Head>
-        <title>Website Starter</title>
+        <title>{appconfig.siteInfo.title}</title>
+        <meta property='og:url' content={appconfig.siteInfo.domain} />
+        <meta property='og:type' content='websitesss' />
+        <meta property='og:title' content={appconfig.siteInfo.title} />
+        <meta property='og:description' content={appconfig.siteInfo.description} />
+        <meta
+          property='og:image'
+          content={`${appconfig.siteInfo.domain}/images/logo/logo.svg`}
+        />
+        <meta property='og:image:alt' content={`${appconfig.siteInfo.name} logo`} />
       </Head>
 
       <HomePageContent />

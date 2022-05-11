a69372693207ae864a6c5cf3ef9b079f30a69ba0
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-05-11 12:49:49 -0400

    get data

---
+++ src/lib/utils/data/getData/getData.ts
@@ -0,0 +1,8 @@
+type GetDataProps = { stubs: any[]; getFn: any; forceProdEnv?: boolean }
+
+export default async function getData({ stubs, getFn }: GetDataProps) {
+  const dataSourceIsDev = process.env.DATA_SOURCE === 'dev'
+  const data = dataSourceIsDev ? stubs : await getFn()
+
+  return data
+}

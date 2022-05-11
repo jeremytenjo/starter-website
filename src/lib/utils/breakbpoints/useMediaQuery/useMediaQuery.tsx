a4bd00754516a883740a87170de277543430a846
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-05-11 17:45:34 -0400

    added useMediaQuery

---
+++ src/lib/utils/breakbpoints/useMediaQuery/useMediaQuery.tsx
@@ -0,0 +1,14 @@
+import { useTheme } from '@mui/material/styles'
+import useMediaQueryMui from '@mui/material/useMediaQuery'
+
+export type UseMediaQueryProps = {
+  type: 'up' | 'down' | 'only' | 'not' | 'between'
+  size: 'xs' | 'sm' | 'md' | 'lg' | 'xl'
+}
+
+export default function useMediaQuery({ type = 'up', size = 'sm' }: UseMediaQueryProps) {
+  const theme = useTheme()
+  const matches = useMediaQueryMui(theme.breakpoints[type](size, null as any))
+
+  return { matches, breakpoints: theme.breakpoints }
+}

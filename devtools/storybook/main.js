fce830e576d4b818b657b9ea836ddb41ed292950
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-05-12 12:32:46 -0400

    storyStoreV7

--- devtools/storybook/main.js
+++ devtools/storybook/main.js
@@ -1,7 +1,7 @@
 module.exports = {
   stories: ['../../src/**/*.stories.mdx', '../../src/**/*.stories.@(js|jsx|ts|tsx)'],
   staticDirs: ['../../public'],
-  features: { emotionAlias: false },
+  features: { emotionAlias: false, storyStoreV7: true },
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-vite',

5359851d4e37f927f342e4f62be3c6b10388a71a
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-05-12 23:38:31 -0400

    FetchTester

--- src/lib/components/nextjs/NextJsApiTester/NextJsApiTester.tsx
+++ src/lib/components/nextjs/NextJsApiTester/NextJsApiTester.tsx
@@ -1,27 +1,12 @@
 import React from 'react'
-import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
-import LinearProgress from '@mui/material/LinearProgress'
-import useFetch, { type RequestProps } from '@useweb/use-fetch'
+import { type RequestProps } from '@useweb/use-fetch'
 
-import ReactJson from '../../ReactJson/ReactJson'
+import FetchTester from '../../data/FetchTester/FetchTester'
 
 type Props = { name: string; payload?: RequestProps }
 
 export default function NextjsApiTester({ name, payload }: Props) {
-  const func = useFetch({ url: `http://localhost:3000/api/${name}` })
+  const url = `http://localhost:3000/api/${name}`
 
-  return (
-    <Box sx={{ p: 2 }}>
-      <Button onClick={() => func.request(payload)} sx={{ mb: 2 }} variant='contained'>
-        Execute
-      </Button>
-
-      {func.fetching && <LinearProgress sx={{ width: '100%', mb: 2 }} />}
-
-      {func.error && <p>{func.error.toString()}</p>}
-
-      {func.response && <ReactJson json={func.response} />}
-    </Box>
-  )
+  return <FetchTester url={url} payload={payload} />
 }

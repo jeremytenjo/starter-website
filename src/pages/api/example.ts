c3465f94b856d3919cdf61de37296795b99c678f
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-05-12 22:23:35 -0400

    import type { NextApiRequest, NextApiResponse } from 'next'  export default function handler(req: NextApiRequest, res: NextApiResponse) {

--- src/pages/api/example.ts
+++ src/pages/api/example.ts
@@ -1,4 +1,6 @@
-export default function handler(req, res) {
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     res.status(200).json({ sucess: 'hello from the example api :)' })
   } catch (error: any) {

17a2fe2741d3aab83e9efacc40340cc138ece07a
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-05-12 21:28:08 -0400

    added example api

---
+++ src/lib/components/nextjs/NextJsApiTester/NextJsApiTester.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import LinearProgress from '@mui/material/LinearProgress'
+import useFetch from '@useweb/use-fetch'
+
+import ReactJson from '../../ReactJson/ReactJson'
+
+type Props = { name: string; payload?: any }
+
+export default function NextjsApiTester({ name, payload }: Props) {
+  const func = useFetch({ url: `http://localhost:3000/api/${name}` })
+
+  return (
+    <Box sx={{ p: 2 }}>
+      <Button onClick={() => func.request(payload)} sx={{ mb: 2 }} variant='contained'>
+        Execute
+      </Button>
+
+      {func.fetching && <LinearProgress sx={{ width: '100%' }} />}
+
+      {func.error && <p>{func.error.toString()}</p>}
+
+      {func.response && <ReactJson json={func.response} />}
+    </Box>
+  )
+}
---
+++ src/tests/api.tests/exampleApi.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import NextjsApiTester from '../../lib/components/nextjs/NextJsApiTester/NextJsApiTester'
+
+export default {
+  title: 'api/Api Example',
+}
+
+export const Test = () => <NextjsApiTester name='example' />

4ad55d4dfe9f58fbe70d17fa3e13714a89e25fce
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-05-12 21:44:50 -0400

    err

--- src/pages/api/example.ts
+++ src/pages/api/example.ts
@@ -1,3 +1,7 @@
 export default function handler(req, res) {
-  res.status(200).json({ sucess: 'hello from the example api :)' })
+  try {
+    res.status(200).json({ sucess: 'hello from the example api :)' })
+  } catch (error: any) {
+    res.status(500).json({ error: error.toString() })
+  }
 }

faa05fd9464112cc19e289d550cc24b1af34b695
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-05-13 20:20:03 -0400

    required

--- src/lib/components/forms/fields/TextField/TextField.tsx
+++ src/lib/components/forms/fields/TextField/TextField.tsx
@@ -3,6 +3,7 @@ import Box, { type BoxProps } from '@mui/material/Box'
 import { IconButton } from '@mui/material'
 import { useFormContext } from 'react-hook-form'
 
+import Text from '../../../Text/Text'
 import IconxSmall from '../../../icons/IconxSmall'
 
 type Props = {
@@ -11,6 +12,7 @@ type Props = {
   wrapperSx?: BoxProps['sx']
   id?: string
   inputProps?: object
+  required?: string
 }
 
 export default function TextField({
@@ -19,11 +21,20 @@ export default function TextField({
   inputProps = {},
   placeholder = '',
   id,
+  required,
 }: Props) {
   const inputRef = useRef<any>(null)
-  const { register, watch, setValue } = useFormContext()
-  const { ref, ...restRegister } = register(name)
+  const {
+    register,
+    watch,
+    setValue,
+    formState: { errors = {} },
+  } = useFormContext()
+  const { ref, ...restRegister } = register(name, {
+    required,
+  })
   const value = watch(name)
+  const error = errors[name]
 
   const clearInput = () => {
     setValue(name, '', { shouldValidate: true })
@@ -32,50 +43,66 @@ export default function TextField({
 
   return (
     <Box
-      id={id}
       sx={{
         display: 'grid',
-        backgroundColor: 'white.main',
-        padding: '4px',
-        width: 'fit-content',
-        borderRadius: '9px',
-        transition: '0.2s',
-        border: '2px solid white',
-        gridAutoFlow: 'column',
-        justifyContent: 'space-between',
-        gridTemplateColumns: '1fr fit-content(100%)',
-        gap: 1,
-        '&:focus': {
-          borderColor: 'primary.main',
-        },
         ...wrapperSx,
       }}
     >
       <Box
-        component='input'
-        placeholder={placeholder}
-        {...restRegister}
-        ref={(e) => {
-          ref(e)
-          inputRef.current = e
-        }}
+        id={id}
         sx={{
-          outline: 'none',
-          border: 'none',
-          fontSize: '16px',
-        }}
-        {...inputProps}
-      />
-
-      <IconButton
-        aria-label='reset'
-        onClick={clearInput}
-        sx={{
-          visibility: value !== '' ? 'visible' : 'hidden',
+          display: 'grid',
+          backgroundColor: 'white.main',
+          padding: '4px',
+          width: 'fit-content',
+          borderRadius: '9px',
+          transition: '0.2s',
+          border: '2px solid white',
+          gridAutoFlow: 'column',
+          justifyContent: 'space-between',
+          gridTemplateColumns: '1fr fit-content(100%)',
+          gap: 1,
+          '&:focus': {
+            borderColor: 'primary.main',
+          },
         }}
       >
-        <IconxSmall sx={{ width: '18px', height: '18px' }} />
-      </IconButton>
+        <Box
+          component='input'
+          placeholder={placeholder}
+          {...restRegister}
+          ref={(e) => {
+            ref(e)
+            inputRef.current = e
+          }}
+          sx={{
+            outline: 'none',
+            border: 'none',
+            fontSize: '16px',
+          }}
+          {...inputProps}
+        />
+
+        <IconButton
+          aria-label='reset'
+          onClick={clearInput}
+          sx={{
+            visibility: value !== '' ? 'visible' : 'hidden',
+          }}
+        >
+          <IconxSmall sx={{ width: '18px', height: '18px' }} />
+        </IconButton>
+      </Box>
+
+      {error && error.type === 'required' && value === '' && (
+        <Text
+          text={error.message}
+          sx={{
+            color: 'error.main',
+            mt: 2,
+          }}
+        />
+      )}
     </Box>
   )
 }

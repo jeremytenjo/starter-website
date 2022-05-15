c45d34ac3e563f8e73397f098088d7b0c7c7146f
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-05-15 12:03:23 -0400

    imagenext handle

--- devtools/storybook/preview.jsx
+++ devtools/storybook/preview.jsx
@@ -1,23 +1,9 @@
 import React from 'react'
 import { create } from '@storybook/theming'
-import * as NextImage from 'next/image'
 
 import ClickToComponent from '../../src/lib/components/ClickToComponent/ClickToComponent'
 
 import StorybookTheme from './theme/storybookTheme'
-
-const OriginalNextImage = NextImage.default
-
-Object.defineProperty(NextImage, 'default', {
-  configurable: true,
-  value: (props) => (
-    <OriginalNextImage
-      {...props}
-      unoptimized
-      blurDataURL='data:image/jpeg;base64,/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAADAAQDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAf/xAAbEAADAAMBAQAAAAAAAAAAAAABAgMABAURUf/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAFxEAAwEAAAAAAAAAAAAAAAAAAAECEf/aAAwDAQACEQMRAD8Anz9voy1dCI2mectSE5ioFCqia+KCwJ8HzGMZPqJb1oPEf//Z'
-    />
-  ),
-})
 
 const theme = create({
   base: 'light',

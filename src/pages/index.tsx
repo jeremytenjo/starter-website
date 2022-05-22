c2eefa9ab935b426d7035d5addfc932109374324
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-05-22 12:17:19 -0400

    added starter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,34 @@
 import React from 'react'
 
+import getMainLayoutData from '../data/getMainLayoutData/getMainLayoutData'
 import HomePageContent from '../content/HomePage/HomePage'
 import { HomePagePropsProvider } from '../content/HomePage/useHomePageProps/useHomePageProps'
+import MainLayout from '../lib/layouts/MainLayout/MainLayout'
+import getProductCategories from '../data/products/productCategory/productCategories.api/getProductCategories'
+import getPopularProducts from '../data/products/getPopularProducts/getPopularProducts'
 
 export default function HomePage(props) {
   return (
     <>
       <HomePagePropsProvider pageProps={props}>
-        <HomePageContent />
+        <MainLayout {...props.mainLayoutData}>
+          <HomePageContent />
+        </MainLayout>
       </HomePagePropsProvider>
     </>
   )
+}
+
+export async function getStaticProps({ previewData }) {
+  const mainLayoutData = await getMainLayoutData({ previewData })
+  const productCategories = await getProductCategories({ previewData })
+  const popularProducts = await getPopularProducts({ previewData })
+
+  return {
+    props: {
+      mainLayoutData,
+      productCategories,
+      popularProducts,
+    },
+  }
 }

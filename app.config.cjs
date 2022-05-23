70697c30d628fe654cd365b4dd2276a27769f38b
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-05-22 22:10:02 -0400

    domain

--- app.config.cjs
+++ app.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   siteInfo: {
     name: 'Website_Starter',
-    domain: 'https://starter-website.vercel.app/',
+    domain: 'https://www.starter-website.vercel.app/',
     description: 'Website_StarterDesc',
     title: 'Website_Starter',
   },

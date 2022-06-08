08b991c4fa4895cc013e857023eba7416a3085e9
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-06-07 20:25:15 -0400

    org

--- devtools/codeGenerator/qcc.schema.cjs
+++ devtools/codeGenerator/qcc.schema.cjs
@@ -10,7 +10,6 @@ const page = require('./templates/page.cjs')
 const pageContent = require('./templates/pageContent.cjs')
 const globalState = require('./templates/globalState.cjs')
 const asyncHook = require('./templates/asyncHook.cjs')
-const cloudFunction = require('./templates/cloudFunction.cjs')
 const muiOverride = require('./templates/muiOverride.cjs')
 const muiOverrideWithStory = require('./templates/muiOverrideWithStory.cjs')
 const playwrightTest = require('./templates/playwrightTest.cjs')
@@ -22,21 +21,28 @@ const apiFunction = require('./templates/apiFunction.cjs')
 
 module.exports = [
   container.template,
+
   component.template,
-  story.template,
   componentWithStory.template,
+
+  story.template,
+
   functions.template,
+  apiFunction.template,
+
   data.template,
+  globalState.template,
+
   playwrightTest.template,
+  vitestTest.template,
+
   muiOverride.template,
   muiOverrideWithStory.template,
+
   page.template,
   pageContent.template,
-  globalState.template,
+
   reactContext.template,
   asyncHook.template,
-  cloudFunction.template,
-  vitestTest.template,
   script.template,
-  apiFunction.template,
 ]

5b7d7f520b0d7a6ba020da9f3511a644b6028d39
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-06-11 20:27:36 -0400

    lazy import @useweb/click-to-component

--- src/lib/components/ClickToComponent/ClickToComponent.ts
+++ src/lib/components/ClickToComponent/ClickToComponent.ts
@@ -1,3 +1,5 @@
-import ClickToComponent from '@useweb/click-to-component'
+import dynamic from 'next/dynamic'
+
+const ClickToComponent = dynamic(() => import('@useweb/click-to-component'))
 
 export default process.env.NODE_ENV === 'development' ? ClickToComponent : () => null

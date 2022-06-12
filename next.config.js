9447214365d7dabf4b9ffa61931e68e094412e31
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-06-11 20:06:26 -0400

    next/bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
-export default {
+import withBundleAnalyzer from '@next/bundle-analyzer'
+
+const withBundleAnalyzerFn = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+export default withBundleAnalyzerFn({
   images: {
     domains: ['images.prismic.io'],
   },
-}
+})

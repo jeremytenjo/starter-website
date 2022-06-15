876d6ee9d027d6ba57b7ec35a6a590993b447811
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-06-15 19:45:08 -0400

    buildapp

---
+++ scripts/buildApp/buildApp.ts
@@ -0,0 +1,7 @@
+import shell from '../../devtools/utils/node/shell.js'
+
+export default function buildApp() {
+  const commands = [`node --experimental-json-modules node_modules/.bin/next build`]
+
+  shell(commands)
+}

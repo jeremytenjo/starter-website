85f17e6fd7a51a3f99c00b87ffe0370170f074f8
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-06-21 13:12:36 -0400

    isProductionSite

---
+++ src/lib/utils/_unique/isProductionSite/isProductionSite.ts
@@ -0,0 +1,13 @@
+import appConfig from '../../../../../app.config.cjs'
+
+export default function isProductionSite() {
+  if (typeof window === 'undefined') return false
+
+  const domainWithoutWWW = appConfig.siteInfo.domain.replace('www.', '')
+
+  const isProdWebsite =
+    window.location.href.includes(appConfig.siteInfo.domain) ||
+    window.location.href.includes(domainWithoutWWW)
+
+  return isProdWebsite
+}

a1028900d30f443f604ba78f8fd3ed50e5e728ea
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-06-28 09:33:32 -0400

    clean

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,7 @@ module.exports = {
     node: true,
     es6: true,
   },
-  ignorePatterns: ['node_modules', 'build', '.env', 'ios', 'android', 'dist'],
+  ignorePatterns: ['node_modules', 'build', '.env', 'dist'],
   plugins: ['react', 'react-hooks', 'import'],
   extends: [
     'next/core-web-vitals',

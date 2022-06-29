66356368aff74bef49f1235beaefc44a3e7dfa71
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-06-29 10:34:19 -0400

    data

--- devtools/codeGenerator/templates/pageContent.cjs
+++ devtools/codeGenerator/templates/pageContent.cjs
@@ -31,7 +31,7 @@ export default function ${name}Content() {
       return `import React, { createContext, useContext } from 'react'
 
       export type ${typesName} = {
-        items: any[]
+        data: any[]
       }
       
       export const ${upperName}Context = createContext<${typesName}>(null as any)

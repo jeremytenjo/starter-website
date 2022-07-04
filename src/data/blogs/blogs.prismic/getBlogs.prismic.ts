15d8e30fe8a71a105caa1f94f243f99132619da5
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-07-04 18:40:03 -0400

    GetBlogsFromPrismicProps

--- src/data/blogs/blogs.prismic/getBlogs.prismic.ts
+++ src/data/blogs/blogs.prismic/getBlogs.prismic.ts
@@ -1,6 +1,8 @@
 import prismicClient from '../../../lib/integrations/Prismic/utils/prismicClient/prismicClient'
 import type BlogSchema from '../blog.schema'
 
+export type GetBlogsFromPrismicProps = any
+
 export default async function getBlogs({ previewData = {} } = {}) {
   const blogs: BlogSchema[] = (await prismicClient({
     previewData,

1bd69527e15745358ee6d3f11c3d1249404c0f2b
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-07-05 13:58:43 -0400

    if (!prismicConfig.accessToken) {     console.log('Prismic config missing accessToken')     return []   }

--- src/data/products/products.prismic/getProductCategories.prismic.ts
+++ src/data/products/products.prismic/getProductCategories.prismic.ts
@@ -1,7 +1,13 @@
 import prismicClient from '../../../lib/integrations/Prismic/utils/prismicClient/prismicClient'
 import type ProductCategorySchema from '../productCategory/productCategory.schema'
+import prismicConfig from '../../../services/prismic/prismic.config'
 
 export default async function getProductCategories({ previewData = {} }) {
+  if (!prismicConfig.accessToken) {
+    console.log('Prismic config missing accessToken')
+    return []
+  }
+
   const productCategories: ProductCategorySchema[] = (await prismicClient({
     previewData,
   }).getAllByType('product-category')) as ProductCategorySchema[]
--- src/data/products/products.prismic/getProducts.prismic.ts
+++ src/data/products/products.prismic/getProducts.prismic.ts
@@ -1,7 +1,13 @@
 import prismicClient from '../../../lib/integrations/Prismic/utils/prismicClient/prismicClient'
 import type ProductSchema from '../product.schema'
+import prismicConfig from '../../../services/prismic/prismic.config'
 
 export default async function getProducts({ previewData = {} } = {}) {
+  if (!prismicConfig.accessToken) {
+    console.log('Prismic config missing accessToken')
+    return []
+  }
+
   const products: ProductSchema[] = (await prismicClient({
     previewData,
   }).getAllByType('product')) as ProductSchema[]

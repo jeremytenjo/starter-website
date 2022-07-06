b4a104cebea839eb458bae83ac88cfe97e23bee0
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-07-06 04:41:38 -0400

    haed

--- src/lib/layouts/Root/containers/RootHeader/RootHeader.tsx
+++ src/lib/layouts/Root/containers/RootHeader/RootHeader.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import Box from '@useweb/ui/Box'
 
-import RootHeaderUi from './RootHeaderUi/RootHeader.ui'
+import MobileHeader from './containers/MobileHeader/MobileHeader'
+import DesktopHeader from './containers/DesktopHeader/DesktopHeader'
 
-export default function RootHeader() {
-  return <RootHeaderUi />
+export type RootHeaderProps = any
+
+export default function RootHeader(props: RootHeaderProps) {
+  return (
+    <Wrapper>
+      <MobileHeader />
+      <DesktopHeader />
+    </Wrapper>
+  )
+}
+
+const Wrapper = ({ children }) => {
+  return <Box data-id='Header'>{children}</Box>
 }

9356d0ccd8f52e896b618bfa55ac54b9de202f8a
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-07-06 07:51:35 -0400

    rootlyou

--- devtools/codeGenerator/templates/page.cjs
+++ devtools/codeGenerator/templates/page.cjs
@@ -12,22 +12,33 @@ import Head from 'next/head'
 
 import ${contentPageName}Content from '../../content/${contentPageName}/${contentPageName}'
 import { ${propsProviderName} } from '../../content/${contentPageName}/use${contentPageName}Props/use${contentPageName}Props'
+import getrootLayoutData from '../data/_root/getRootLayoutData/getRootLayoutData'
 
 export type ${propsName} = {
   data: any[]
 }
 
-    export default function ${upperName}Page(props: ${propsName}) {
-      return <>
-       <Head>
-         <title>${upperName}</title>
-       </Head>
-      
-       <${propsProviderName} pageProps={props} >
-        <${contentPageName}Content />
-       </${propsProviderName}>
-      </>
-    }`
+export default function ${upperName}Page(props: ${propsName}) {
+  return <>
+   <Head>
+     <title>${upperName}</title>
+   </Head>
+  
+   <${propsProviderName} pageProps={props} >
+    <${contentPageName}Content />
+   </${propsProviderName}>
+  </>
+}
+
+export async function getStaticProps({ previewData }) {
+  const rootLayoutData = await getrootLayoutData({ previewData })
+
+  return {
+    props: {
+      rootLayoutData,
+    },
+  }
+}`
     },
   },
 ]

55c1bdec37ef95da1b66d7e7a5fe660bc328f416
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-07-08 12:09:19 -0400

    renamed to websiteCodeGenTemaplates

--- devtools/codeGenerator/superCodeGen.schema.cjs
+++ devtools/codeGenerator/superCodeGen.schema.cjs
@@ -1,6 +1,6 @@
 // templates for the `Super Code Generator` vscode plugin
 // https://marketplace.visualstudio.com/items?itemName=tenjojeremy.super-code-generator
 
-const websiteTemaplates = require('@useweb/starter-website-code-gen-templates/superCodeGen.schema.cjs')
+const websiteCodeGenTemaplates = require('@useweb/starter-website-code-gen-templates/superCodeGen.schema.cjs')
 
-module.exports = [...websiteTemaplates]
+module.exports = [...websiteCodeGenTemaplates]

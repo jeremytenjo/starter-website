974e0910781a5c70b4196cee96d1919a66e9cbbe
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-07-14 11:19:12 -0400

    sx

--- src/lib/components/basic/misc/SiteLogo/SiteLogo.tsx
+++ src/lib/components/basic/misc/SiteLogo/SiteLogo.tsx
@@ -2,16 +2,22 @@ import React from 'react'
 
 import appConfig from '../../../../../../app.config.cjs'
 import { useRootLayoutData } from '../../../../layouts/Root/RootLayout'
-import Link from '../Link/Link'
+import Link, { type LinkProps } from '../Link/Link'
 import Image from '../Image/Image'
 
 export type SiteLogoProps = {
   src?: string
   width?: string | number
   height?: string | number
+  sx?: LinkProps['sx']
 }
 
-export default function SiteLogo({ src, width = 40, height = 40 }: SiteLogoProps) {
+export default function SiteLogo({
+  src,
+  width = 40,
+  height = 40,
+  sx = {},
+}: SiteLogoProps) {
   const rootData = useRootLayoutData()
   const siteLogo = src || rootData?.settings?.data?.logo?.url || '/images/logo/logo.svg'
 
@@ -20,9 +26,7 @@ export default function SiteLogo({ src, width = 40, height = 40 }: SiteLogoProps
       href='/'
       data-id='Logo'
       sx={{
-        display: 'grid',
-        justifyContent: 'center',
-        gap: '12px',
+        ...sx,
       }}
     >
       <Image

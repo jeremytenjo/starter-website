4320049d805659539da2a54493b087cb34d90c36
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-07-19 15:04:17 -0400

    d

--- devtools/codeGenerator/packageTemplates/templates/page.cjs
+++ devtools/codeGenerator/packageTemplates/templates/page.cjs
@@ -7,15 +7,14 @@ const files = [
     path: () => `index.tsx`,
     template: ({ name, helpers }) => {
       const upperName = helpers.changeCase.capitalCase(name).split(' ').join('')
-      const contentPageName = `${upperName}Page`
-      const propsProviderName = `${contentPageName}PropsProvider`
+      const propsProviderName = `${upperName}PropsProvider`
       const propsName = `${helpers.changeCase.pascalCase(name).split(' ').join('')}Props`
 
       return `import React from 'react' 
 import Head from 'next/head'
 
-import ${contentPageName}Content from '../../pagesContent/${contentPageName}/${contentPageName}'
-import { ${propsProviderName} } from '../../pagesContent/${contentPageName}/use${contentPageName}Props/use${contentPageName}Props'
+import ${upperName}Content from '../../pagesContent/${upperName}/${upperName}'
+import { ${propsProviderName} } from '../../pagesContent/${upperName}/use${upperName}Props/use${upperName}Props'
 import getrootLayoutData from '../data/_root/getRootData/getRootData'
 import RootLayout, { type RootLayoutProps } from '../lib/layouts/Root/RootLayout'
 
@@ -31,7 +30,7 @@ export default function ${upperName}Page(props: ${propsName}) {
   
    <RootLayout rootLayoutData={props.rootLayoutData}>
     <${propsProviderName} pageProps={props} >
-      <${contentPageName}Content />
+      <${upperName}Content />
     </${propsProviderName}>
    </RootLayout>
 

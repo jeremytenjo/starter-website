fc6e2c99ef4aa58854f221859422ac3833fd2f7c
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-07-19 13:48:13 -0400

    org

--- devtools/codeGenerator/appGenerator/generateApp/handlers/handlePages/handlePages.ts
+++ devtools/codeGenerator/appGenerator/generateApp/handlers/handlePages/handlePages.ts
@@ -1,6 +1,5 @@
 import path from 'path'
 
-import createFile from '../../../../../utils/node/createFile.js'
 import { type ContainerProps } from '../../sharedTypes'
 import { type ContextProps } from '../../generateApp'
 import genCodeFromTemplate from '../../utils/genCodeFromTemplate/genCodeFromTemplate.js'
@@ -20,10 +19,9 @@ export default async function handlePages({ pages = [], context }: PagesProps) {
 
   await Promise.all(
     pages.map(async (page: PagesProps['pages'][0]) => {
-      // console.log(page)
       await genCodeFromTemplate({
-        files: context.templates.page,
         name: page.name,
+        files: context.templates.page,
         outputPath: pagesDir,
       })
     }),

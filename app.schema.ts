54eb08011027a6b7ace87d249b1407d246862088
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-07-20 09:06:21 -0400

    pretty

--- app.schema.ts
+++ app.schema.ts
@@ -1,10 +1,18 @@
+// open devtools/codeGenerator/appGenerator/test/testAppSchema.ts for an example
 import superCodeGenSchema from './devtools/codeGenerator/superCodeGen.schema.cjs'
 import {
   type SchemaProps,
   type ElementsTypes,
 } from './devtools/codeGenerator/appGenerator/generateApp/generateApp'
 
-// open devtools/codeGenerator/appGenerator/test/testAppSchema.ts for an example
+const templates = {
+  page: superCodeGenSchema.find((s) => s.type === 'Page')?.files || [],
+  pageContent: superCodeGenSchema.find((s) => s.type === 'Page Content')?.files || [],
+  container: superCodeGenSchema.find((s) => s.type === 'Container')?.files || [],
+  component:
+    superCodeGenSchema.find((s) => s.type === 'Component with story')?.files || [],
+  data: superCodeGenSchema.find((s) => s.type === 'Data')?.files || [],
+}
 
 const pages = (): ElementsTypes['PagesProps']['pages'] => {
   return []
@@ -18,15 +26,6 @@ const data = (): ElementsTypes['DataProps']['data'] => {
   return []
 }
 
-const templates = {
-  page: superCodeGenSchema.find((s) => s.type === 'Page')?.files || [],
-  pageContent: superCodeGenSchema.find((s) => s.type === 'Page Content')?.files || [],
-  container: superCodeGenSchema.find((s) => s.type === 'Container')?.files || [],
-  component:
-    superCodeGenSchema.find((s) => s.type === 'Component with story')?.files || [],
-  data: superCodeGenSchema.find((s) => s.type === 'Data')?.files || [],
-}
-
 export default async function appSchema() {
   const schema: SchemaProps = {
     templates,
@@ -34,6 +33,5 @@ export default async function appSchema() {
     data,
     components,
   }
-
   return schema
 }

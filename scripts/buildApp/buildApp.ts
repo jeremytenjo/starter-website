d1c3e1c9c893448c462da3f2e1c1d9a262390d4c
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-07-21 09:06:13 -0400

    added sitemap gen helper

--- scripts/buildApp/buildApp.ts
+++ scripts/buildApp/buildApp.ts
@@ -1,14 +1,7 @@
 import shell from '../../devtools/utils/node/shell.js'
 
 export default function buildApp() {
-  let commands = `node --experimental-json-modules node_modules/.bin/next build && node node_modules/.bin/next-sitemap --config next-sitemap.cjs`
-
-  if (process.env.NODE_GIT_STAGE === 'prepush') {
-    commands = commands.replace(
-      '&& node node_modules/.bin/next-sitemap --config next-sitemap.cjs',
-      '',
-    )
-  }
+  const commands = `node --experimental-json-modules node_modules/.bin/next build`
 
   shell(commands)
 }

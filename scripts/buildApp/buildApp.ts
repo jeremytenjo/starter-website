8a48b6eb6cc3c2536bff89e61ef8215f5958df2e
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-07-20 22:28:29 -0400

    pre

--- scripts/buildApp/buildApp.ts
+++ scripts/buildApp/buildApp.ts
@@ -1,7 +1,14 @@
 import shell from '../../devtools/utils/node/shell.js'
 
 export default function buildApp() {
-  const commands = [`node --experimental-json-modules node_modules/.bin/next build`]
+  let commands = `node --experimental-json-modules node_modules/.bin/next build && node node_modules/.bin/next-sitemap --config next-sitemap.cjs`
+
+  if (process.env.NODE_GIT_STAGE === 'prepush') {
+    commands = commands.replace(
+      '&& node node_modules/.bin/next-sitemap --config next-sitemap.cjs',
+      '',
+    )
+  }
 
   shell(commands)
 }

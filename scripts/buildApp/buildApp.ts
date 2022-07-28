07f2ddd6d86a40eb11b2324b35dc8202337be633
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-07-28 18:24:06 -0400

    helpers:generate-sitemap

--- scripts/buildApp/buildApp.ts
+++ scripts/buildApp/buildApp.ts
@@ -4,7 +4,7 @@ export default function buildApp() {
   let commands = `node --experimental-json-modules node_modules/.bin/next build`
 
   if (process.env.COMMIT_SITEMAP) {
-    commands = `${commands} && git add public/sitemap.xml && git commit -m "updated sitemap"`
+    commands = `${commands} && npm run helpers:generate-sitemap && git add public/sitemap.xml && git commit -m "updated sitemap"`
   }
 
   shell(commands)

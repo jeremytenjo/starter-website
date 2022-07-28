0d1a8856a6e790674192649b932496cf11af3ee5
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-07-28 18:27:08 -0400

    commands

--- scripts/buildApp/buildApp.ts
+++ scripts/buildApp/buildApp.ts
@@ -1,10 +1,12 @@
 import shell from '../../devtools/utils/node/shell.js'
 
 export default function buildApp() {
-  let commands = `node --experimental-json-modules node_modules/.bin/next build`
+  const commands = [`node --experimental-json-modules node_modules/.bin/next build`]
 
   if (process.env.COMMIT_SITEMAP) {
-    commands = `${commands} && npm run helpers:generate-sitemap && git add public/sitemap.xml && git commit -m "updated sitemap"`
+    commands.push(
+      `npm run helpers:generate-sitemap && git add public/sitemap.xml && git commit -m "updated sitemap"`,
+    )
   }
 
   shell(commands)

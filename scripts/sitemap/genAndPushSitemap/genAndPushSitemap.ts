d6dff7bb9ed8c0f44fb2053b1eed20187b89bd43
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-07-29 08:39:26 -0400

    a

---
+++ scripts/sitemap/genAndPushSitemap/genAndPushSitemap.ts
@@ -0,0 +1,7 @@
+import shell from '../../../devtools/utils/node/shell.js'
+
+export default async function genAndPushSitemap() {
+  shell(
+    'npm run helpers:generate-sitemap && git add public/sitemap.xml && git commit -m "updated sitemap"',
+  )
+}

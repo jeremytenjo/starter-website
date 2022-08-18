9a8b3ef2ec6583a5fd8370b4485b87385e75c7eb
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-08-18 12:06:17 -0400

    a

--- devtools/codeGenerator/packageTemplates/templates/apiFunctionTester.cjs
+++ devtools/codeGenerator/packageTemplates/templates/apiFunctionTester.cjs
@@ -1,7 +1,7 @@
 // https://github.com/jeremytenjo/super-code-generator/tree/master#component-type-properties
 const files = [
   {
-    path: ({ name }) => `${name}.stories.ts`,
+    path: ({ name }) => `${name}.stories.tsx`,
     template: ({ name }) => {
       return `import React from 'react'
       import NextApiTester from '@useweb/next-api-tester'

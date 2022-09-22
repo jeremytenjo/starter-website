cfc9504088fc6ea4b0b4f0e64e04600f1d8101b3
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-09-22 18:02:49 -0400

    pge

--- src/lib/integrations/Prismic/utils/linkResolver/linkResolver.ts
+++ src/lib/integrations/Prismic/utils/linkResolver/linkResolver.ts
@@ -2,8 +2,8 @@
 export default function linkResolver(doc) {
   // doc type is equivalent to prismic doc type
   switch (doc.type) {
-    case 'product':
-      return `/products/${doc.slug}`
+    case 'page':
+      return `/${doc.uid}`
     case 'product-category':
       return `/categories/${doc.slub}`
     default:

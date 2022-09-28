464d53d880fdfc2009a86e6882e53cfbcf5a6cc3
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-09-28 14:12:39 -0400

    @useweb/figma-to-code

--- scripts/design/figma/figmaToCode/figmaToCode.ts
+++ scripts/design/figma/figmaToCode/figmaToCode.ts
@@ -1,21 +1,14 @@
 import dotenv from 'dotenv'
-import * as Figma from 'figma-api'
 
-import assert from '../../../../src/lib/utils/misc/assert/assert.js'
+// replace with @useweb/figma-to-code
+// import generateCodeFromFigma from './handlers/generateCodeFromFigma/generateCodeFromFigma.js'
 
 dotenv.config({ path: '.env.local' })
 
 export default async function figmaToCode() {
-  const personalAccessToken: string = process.env.FIGMA_API_PERSONAL_ACCESS_TOKEN as any
-
-  assert({
-    condition: Boolean(personalAccessToken),
-    message: 'Missing FIGMA_API_PERSONAL_ACCESS_TOKEN in .env.local',
-  })
-
-  const api = new Figma.Api({
-    personalAccessToken,
-  })
-
-  const file = await api.getFile('my file key')
+  // const personalAccessToken: string = process.env.FIGMA_API_PERSONAL_ACCESS_TOKEN as any
+  // await generateCodeFromFigma({
+  //   personalAccessToken,
+  //   figmaFileId: 'foGTHj1byfaHzvrq08n6Xv',
+  // })
 }

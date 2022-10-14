5cf05b95ab912c6af06a6836041c751accae1560
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-10-14 08:56:08 -0400

    ${returnName}

--- devtools/codeGenerator/packageTemplates/templates/function.cjs
+++ devtools/codeGenerator/packageTemplates/templates/function.cjs
@@ -4,10 +4,15 @@ const files = [
     path: ({ name }) => `${name}.ts`,
     template: ({ name, helpers }) => {
       const propsName = `${helpers.changeCase.capitalCase(name).split(' ').join('')}Props`
+      const returnName = `${helpers.changeCase
+        .capitalCase(name)
+        .split(' ')
+        .join('')}Return`
 
       return `export type ${propsName} = {name: string}
+      export type ${returnName} = any
     
-    export default function ${name}(props: ${propsName}) {
+    export default function ${name}(props: ${propsName}): ${returnName} {
       const data = 'hi'
       
       return { data }

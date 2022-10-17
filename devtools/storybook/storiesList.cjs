3c7242e291098c7b6b33d0dd276a63c207bbcd5f
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-10-17 14:08:15 -0400

    a

--- devtools/storybook/storiesList.cjs
+++ devtools/storybook/storiesList.cjs
@@ -5,6 +5,7 @@
 
 module.exports = [
   '../../src/apiFunctions/exampleApi/stories/exampleApi.stories.tsx',
+  '../../src/lib/components/basic/input/InputFile/stories/InputFile.stories.tsx',
   '../../src/lib/components/basic/misc/CodeBlock/stories/CodeBlock.stories.tsx',
   '../../src/lib/components/basic/misc/Image/stories/Image.stories.tsx',
   '../../src/lib/components/useweb/Alert/stories/Alert.stories.tsx',

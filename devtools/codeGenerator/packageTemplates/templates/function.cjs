ef8482247b57291ee7ca91625705c04eaaf52d31
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-10-17 12:10:09 -0400

    a

--- devtools/codeGenerator/packageTemplates/templates/function.cjs
+++ devtools/codeGenerator/packageTemplates/templates/function.cjs
@@ -11,7 +11,7 @@ const files = [
 
       return `export type ${propsName} = {name: string}
     
-    export default function ${name}(props: ${propsName}): ${returnName} {
+    export default function ${name}(props: ${propsName}) {
       const data = 'hi'
       
       return { data }

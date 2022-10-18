e3ac42e2cbd5359b57e1201654e424e9ebf9944a
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-10-18 15:16:23 -0400

    HomePageContent

--- src/pagesContent/Home/Home.tsx
+++ src/pagesContent/Home/Home.tsx
@@ -1,31 +1,8 @@
 import React from 'react'
 import Box from '@useweb/ui/Box'
-import Form from '@useweb/ui/Form'
-import Select from '@useweb/ui/Select'
-import TextField from '@useweb/ui/Textfield'
 
 export default function HomePageContent() {
-  return (
-    <Wrapper>
-      <Form
-        onSubmit={(dat) => {
-          console.log({ dat })
-        }}
-      >
-        <Select
-          name='holla'
-          label='label'
-          options={[
-            {
-              label: 'asfadf',
-              value: 'holllala',
-            },
-          ]}
-        />
-        <TextField name='name' />
-      </Form>
-    </Wrapper>
-  )
+  return <Wrapper>HomePageContent</Wrapper>
 }
 
 const Wrapper = ({ children }) => {

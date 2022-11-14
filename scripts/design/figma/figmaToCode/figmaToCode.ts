2f92b85ae4421e8e1abbb9c533c92b0388789c57
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-11-14 15:56:36 -0500

    gd

--- scripts/design/figma/figmaToCode/figmaToCode.ts
+++ scripts/design/figma/figmaToCode/figmaToCode.ts
@@ -1,14 +1,21 @@
 import dotenv from 'dotenv'
+import generateCodeFromFigma from '@useweb/figma-to-code'
 
-// replace with @useweb/figma-to-code
-// import generateCodeFromFigma from './handlers/generateCodeFromFigma/generateCodeFromFigma.js'
+import prettierConfig from '../../../../devtools/prettier/prettier.config.json' assert { type: 'json' }
 
 dotenv.config({ path: '.env.local' })
 
 export default async function figmaToCode() {
-  // const personalAccessToken: string = process.env.FIGMA_API_PERSONAL_ACCESS_TOKEN as any
-  // await generateCodeFromFigma({
-  //   personalAccessToken,
-  //   figmaFileId: 'foGTHj1byfaHzvrq08n6Xv',
-  // })
+  const personalAccessToken: string = process.env.FIGMA_API_PERSONAL_ACCESS_TOKEN as any
+
+  await generateCodeFromFigma({
+    personalAccessToken,
+    // https://www.figma.com/file/RtKeMMXoXeIytVrKIFhD5z/Figma-To-Code?node-id=102%3A103 Home Page
+    figmaFileId: 'RtKeMMXoXeIytVrKIFhD5z',
+    pageName: 'Page 1',
+    outputDir: 'generatedComponents',
+    iconsDir: 'generatedComponents/icons',
+    storyTitle: 'design/FigmaToCode',
+    prettierConfig,
+  })
 }

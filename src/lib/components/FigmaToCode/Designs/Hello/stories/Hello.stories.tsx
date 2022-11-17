2649c915635617790290e7df32b0e8dfb689fccc
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-11-16 23:39:02 -0500

    a

--- src/lib/components/FigmaToCode/Designs/Hello/stories/Hello.stories.tsx
+++ src/lib/components/FigmaToCode/Designs/Hello/stories/Hello.stories.tsx
@@ -4,14 +4,14 @@
  */
 
 //https://storybook.js.org/docs/react/writing-docs/docs-page
-import React from "react";
-import PixelPerfect from "@useweb/pixel-perfect";
+import React from 'react'
+import PixelPerfect from '@useweb/pixel-perfect'
 
-import HelloComponent from "../Hello";
+import HelloComponent from '../Hello'
 
 export default {
-  title: "FigmaToCode/Designs",
-};
+  title: 'FigmaToCode/Designs',
+}
 
 export const Hello = {
   render: () => {
@@ -22,12 +22,12 @@ export const Hello = {
           assets={[
             {
               width: 0,
-              url: "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/0446c071-1543-4a55-bda1-63585c3c890c",
+              url: 'https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/0446c071-1543-4a55-bda1-63585c3c890c',
             },
           ]}
         />
         <HelloComponent />
       </>
-    );
+    )
   },
-};
+}

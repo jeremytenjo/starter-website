b86ac11a9f3a6fcba74a2e6d443a2401d12e4352
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-11-20 20:52:34 -0500

    a

--- devtools/figmaToCode/figma-to-code.config.js
+++ devtools/figmaToCode/figma-to-code.config.js
@@ -1,14 +1,13 @@
 import dotenv from 'dotenv'
-import generateCodeFromFigma from '@useweb/figma-to-code'
 
-import prettierConfig from '../../../../devtools/prettier/prettier.config.json' assert { type: 'json' }
+import prettierConfig from '../prettier/prettier.config.json' assert { type: 'json' }
 
 dotenv.config({ path: '.env.local' })
 
-export default async function figmaToCode() {
-  const personalAccessToken: string = process.env.FIGMA_API_PERSONAL_ACCESS_TOKEN as any
+export default async function figmaToCodeConfig() {
+  const personalAccessToken = process.env.FIGMA_API_PERSONAL_ACCESS_TOKEN
 
-  await generateCodeFromFigma({
+  return {
     personalAccessToken,
     // https://www.figma.com/file/c7FWk1Nfl01MRIOhhT3aFi/starter-website?node-id=1%3A2
     figmaFileId: 'c7FWk1Nfl01MRIOhhT3aFi',
@@ -19,5 +18,5 @@ export default async function figmaToCode() {
     storyTitlePrefix: 'FigmaToCode',
     overwrite: true,
     prettierConfig,
-  })
+  }
 }

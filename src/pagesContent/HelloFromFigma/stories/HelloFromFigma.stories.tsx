5a02d9c95bc88b7f2cf48d2e38a5a0de1ff90f6d
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-11-20 21:39:08 -0500

    a

--- src/pagesContent/HelloFromFigma/stories/HelloFromFigma.stories.tsx
+++ src/pagesContent/HelloFromFigma/stories/HelloFromFigma.stories.tsx
@@ -4,14 +4,14 @@
  */
 
 //https://storybook.js.org/docs/react/writing-docs/docs-page
-import React from "react";
-import PixelPerfect from "@useweb/pixel-perfect";
+import React from 'react'
+import PixelPerfect from '@useweb/pixel-perfect'
 
-import HelloFromFigmaComponent from "../HelloFromFigma";
+import HelloFromFigmaComponent from '../HelloFromFigma'
 
 export default {
-  title: "FigmaToCode/pages",
-};
+  title: 'FigmaToCode/pages',
+}
 
 export const HelloFromFigma = {
   render: () => {
@@ -22,12 +22,12 @@ export const HelloFromFigma = {
           assets={[
             {
               width: 0,
-              url: "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/90035a76-99ef-4bd7-a95a-7384c09c6c1e",
+              url: 'https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/90035a76-99ef-4bd7-a95a-7384c09c6c1e',
             },
           ]}
         />
         <HelloFromFigmaComponent />
       </>
-    );
+    )
   },
-};
+}

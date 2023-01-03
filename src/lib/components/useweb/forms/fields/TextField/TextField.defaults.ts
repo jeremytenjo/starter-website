0fba5848173e2117ffd2bc1be3fb1dd7bf7abcc4
Jeremy Tenjo <tenjojeremy@gmail.com>
2023-01-02 21:37:24 -0500

    Field

---
+++ src/lib/components/useweb/forms/fields/TextField/TextField.defaults.ts
@@ -0,0 +1,8 @@
+// https://mui.com/customization/theme-components/#global-style-overrides
+// import in src/theme/mui/MuiProvider.jsx
+import { type ComponentDefaultsProps } from '@useweb/ui-theme'
+import { type TextFieldProps } from '@useweb/ui/TextField'
+
+const defaults: ComponentDefaultsProps<TextFieldProps> = {}
+
+export default defaults

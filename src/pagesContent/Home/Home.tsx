166e5e98f5d8d0bf670ae33622b244000f10e4c7
Jeremy Tenjo <tenjojeremy@gmail.com>
2023-01-06 14:14:26 -0500

    a

--- src/pagesContent/Home/Home.tsx
+++ src/pagesContent/Home/Home.tsx
@@ -1,13 +1,8 @@
 import React from 'react'
 import Box from '@useweb/ui/Box'
-import Text from '@useweb/ui/Text'
 
 export default function HomePageContent() {
-  return (
-    <Wrapper>
-      <Text text={`Home Page`} />
-    </Wrapper>
-  )
+  return <Wrapper>HomePage</Wrapper>
 }
 
 const Wrapper = ({ children }) => {

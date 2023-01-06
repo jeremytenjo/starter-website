c411de7b688defdcf3cbafac51c1159dfdba82cf
Jeremy Tenjo <tenjojeremy@gmail.com>
2023-01-06 09:22:30 -0500

    a

--- scripts/dev/handlers/addEmulatorData/handlers/addFirestoreEmulatorData.ts
+++ scripts/dev/handlers/addEmulatorData/handlers/addFirestoreEmulatorData.ts
@@ -45,15 +45,10 @@ async function getCollectionsData() {
     stubsData.map(async (stubPath) => {
       const [name] = stubPath.split('/').pop()?.split('.') || []
       const { default: data } = await import(stubPath)
-      // documents with uid property are replaced with emulator user id
-      const setUserIdToDataFromSignedInUser = Object.keys(data?.[0] || {})?.includes(
-        'userId',
-      )
 
       return {
         name,
         data,
-        setUserIdToDataFromSignedInUser,
       }
     }),
   )

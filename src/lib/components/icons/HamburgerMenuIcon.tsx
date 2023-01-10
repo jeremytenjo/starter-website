419016be873ccd4c5b7ad8faa6b982b1420fce50
Jeremy Tenjo <tenjojeremy@gmail.com>
2023-01-10 14:16:39 -0500

    c

--- src/lib/components/icons/HamburgerMenuIcon.tsx
+++ src/lib/components/icons/HamburgerMenuIcon.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { createSvgIcon } from '@mui/material'
+import colors from '../../../theme/tokens/colors'
 
 export default createSvgIcon(
   <svg viewBox='0 0 19 13' fill='none' xmlns='http://www.w3.org/2000/svg'>
     <path
       d='M1 0.861816H17.5M1 6.11182H17.5M1 11.3618H17.5'
-      stroke='#040F05'
+      stroke={colors.black.main}
       strokeWidth={1.5}
       strokeLinecap='round'
       strokeLinejoin='round'

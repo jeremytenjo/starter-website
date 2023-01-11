967adb3abab19419751c551048ea85f663b8eae7
Jeremy Tenjo <tenjojeremy@gmail.com>
2023-01-11 18:29:50 -0500

    i.pravatar.cc

--- next.config.js
+++ next.config.js
@@ -16,7 +16,7 @@ export default async () => {
       nextjsPort: String(appConfig.nextjs.port),
     },
     images: {
-      domains: ['images.prismic.io'],
+      domains: ['images.prismic.io', 'i.pravatar.cc'],
     },
   })
 

64cb616d0658ea6062ea20e7e3bf47d14aa09590
Jeremy Tenjo <tenjojeremy@gmail.com>
2023-01-11 14:43:40 -0500

    udpated dialog

--- src/lib/components/useweb/Dialog/Dialog.defaults.ts
+++ src/lib/components/useweb/Dialog/Dialog.defaults.ts
@@ -1,5 +1,5 @@
 // https://mui.com/customization/theme-components/#global-style-overrides
-// import in src/theme/mui/MuiProvider.jsx
+// import in src/theme/UiTheme/UiTheme.jsx
 import { type ComponentDefaultsProps } from '@useweb/ui-theme'
 import { type DialogProps } from '@useweb/ui/Dialog'
 
@@ -7,8 +7,8 @@ const defaults: ComponentDefaultsProps<DialogProps> = {
   styleOverrides: {
     root: {
       '& .MuiPaper-root': {
-        backgroundColor: 'transparent',
-        color: 'white',
+        boxShadow: '0px 9px 38px rgba(0, 0, 0, 0.06)',
+        borderRadius: '20px',
       },
       '& .MuiDialog-scrollPaper': {
         background: 'rgb(255 255 255 / 33%)',

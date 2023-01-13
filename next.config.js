8ffe83aea7083e8673d09654dcd38abe80787432
Jeremy Tenjo <tenjojeremy@gmail.com>
2023-01-13 12:09:14 -0500

    d

--- next.config.js
+++ next.config.js
@@ -16,7 +16,7 @@ export default async () => {
       nextjsPort: String(appConfig.nextjs.port),
     },
     images: {
-      domains: ['images.prismic.io', 'i.pravatar.cc'],
+      domains: ['images.prismic.io', 'i.pravatar.cc', 's3-alpha-sig.figma.com'],
     },
   })
 

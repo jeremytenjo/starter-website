1a493cce71732652e02c29fd693e482a6e650ab9
Jeremy Tenjo <tenjojeremy@gmail.com>
2023-01-15 18:32:25 -0500

    ce

--- src/lib/layouts/CenterColumn/CenterColumn.tsx
+++ src/lib/layouts/CenterColumn/CenterColumn.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import Box, { BoxProps } from '@useweb/ui/Box'
 
-export type CenterColumnProps = { children: any; sx?: BoxProps['sx']; dataId?: string }
+export type CenterColumnProps = {
+  children: any
+  sx?: BoxProps['sx']
+  dataId?: string
+  'data-id'?: string
+}
 
 export default function CenterColumn({
   children,
   sx = {},
   dataId = 'CenterColumn',
+  ...rest
 }: CenterColumnProps) {
   return (
-    <Wrapper sx={sx} dataId={dataId}>
+    <Wrapper sx={sx} dataId={rest['data-id'] || dataId}>
       {children}
     </Wrapper>
   )
@@ -23,7 +29,7 @@ const Wrapper = ({ children, sx, dataId }) => {
         width: '100%',
         display: 'grid',
         margin: '0 auto',
-        maxWidth: '769px',
+        maxWidth: '1483px',
         ...sx,
       }}
     >

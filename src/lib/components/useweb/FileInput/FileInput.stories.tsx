6c2d74cc546ea047585ab2491fdfe9ac8727cc0c
Jeremy Tenjo <tenjojeremy@gmail.com>
2023-01-23 09:48:19 -0500

    uu

--- src/lib/components/useweb/FileInput/FileInput.stories.tsx
+++ src/lib/components/useweb/FileInput/FileInput.stories.tsx
@@ -1,18 +1,18 @@
 /* eslint-disable storybook/prefer-pascal-case */
 import React from 'react'
-import InputFile from '@useweb/ui/InputFile'
+import FileInput from '@useweb/ui/FileInput'
 
 import Typography from '../../../../theme/tokens/stories/theme.typography'
 
 export default {
-  title: 'lib/components/useweb/InputFile',
+  title: 'lib/components/useweb/FileInput',
 }
 
 export const Default = {
   render: (args) => {
     return (
       <>
-        <InputFile {...args} />
+        <FileInput {...args} />
       </>
     )
   },

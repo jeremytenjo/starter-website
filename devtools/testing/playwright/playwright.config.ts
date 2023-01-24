c41637f8e1e4b11287e0dfbc3b2ae2ac9088bc08
Jeremy Tenjo <tenjojeremy@gmail.com>
2023-01-24 08:20:35 -0500

    port

--- devtools/testing/playwright/playwright.config.ts
+++ devtools/testing/playwright/playwright.config.ts
@@ -2,6 +2,7 @@ import path from 'path'
 
 import type { PlaywrightTestConfig } from '@playwright/test'
 import { devices } from '@playwright/test'
+import appConfig from '../../../app.config'
 
 /**
  * Read environment variables from file.
@@ -37,14 +38,14 @@ const config: PlaywrightTestConfig = {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:3000',
+    baseURL: `http://localhost:${appConfig.nextjs.port}`,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     video: 'retain-on-failure',
   },
   webServer: {
     command: 'npm run dev:for-testing',
-    port: 3000,
+    port: appConfig.nextjs.port,
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
     cwd: process.cwd(),

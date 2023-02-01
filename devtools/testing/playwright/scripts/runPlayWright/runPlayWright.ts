1662a8f52cabdc9b321a81d8e743528cd52a944f
Jeremy Tenjo <tenjojeremy@gmail.com>
2023-02-01 10:01:09 -0500

    ests:playwright:single:test

--- devtools/testing/playwright/scripts/runPlayWright/runPlayWright.ts
+++ devtools/testing/playwright/scripts/runPlayWright/runPlayWright.ts
@@ -1,19 +1,49 @@
 import path from 'path'
+// https://github.com/enquirer/enquirer
+import enquirer from 'enquirer'
 
 import watchFolder from '../../../../utils/node/watchFolder.js'
 import shell from '../../../../utils/node/shell.js'
 import getCommandLineArgs from '../../../../utils/node/getCommandLineArgs.js'
+import glob from '../../../../utils/node/glob.js'
+
+const { Select } = enquirer as any
 
 export default async function runPlayWright() {
-  const { watch } = getCommandLineArgs([
+  const { watch, singleTest } = getCommandLineArgs([
     {
       name: 'watch',
       type: Boolean,
     },
+    {
+      name: 'singleTest',
+      type: Boolean,
+    },
   ])
+
+  let singTestCommand = ''
+
+  if (singleTest) {
+    const tests = await glob({
+      pattern: path.join(process.cwd(), 'src', '**', '*.e2e.ts'),
+    })
+    const testsNames = tests.map((t) => t.split('/').pop())
+    const prompt = new Select({
+      name: 'selectedTest',
+      message: 'Select test',
+      choices: testsNames,
+      initial: testsNames[0],
+    })
+
+    console.clear()
+    const selectedTest = await prompt.run()
+    console.log('')
+    singTestCommand = `${selectedTest} --headed`
+  }
 
   await runPlaywrightTests({
     headed: watch,
+    singTestCommand,
   })
 
   if (watch) {
@@ -26,10 +56,15 @@ export default async function runPlayWright() {
   }
 }
 
-const runPlaywrightTests = async ({ headed }: { headed: boolean }) => {
-  const options = `${headed ? '--headed' : ''}`
+type RunPlaywrightTestsProps = {
+  headed: boolean
+  singTestCommand: string
+}
+
+const runPlaywrightTests = async (props: RunPlaywrightTestsProps) => {
+  const options = `${props.headed ? '--headed' : ''}`
 
   shell(
-    `npx playwright test ${options} --config=devtools/testing/playwright/playwright.config.ts`,
+    `npx playwright test ${props.singTestCommand} ${options} --config=devtools/testing/playwright/playwright.config.ts`,
   )
 }

26408815a61cd8c066739e282b9b156849085e97
Jeremy Tenjo <tenjojeremy@gmail.com>
2023-02-02 16:26:54 -0500

    // added in root in order for playwright vs code extension to detect our config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,7 @@
 import type { PlaywrightTestConfig } from '@playwright/test'
 import defaultPlaywrightConfig from './devtools/testing/playwright/playwright.config.js'
 
+// added in root in order for playwright vs code extension to detect our config
 const config: PlaywrightTestConfig = {
   ...defaultPlaywrightConfig,
 }

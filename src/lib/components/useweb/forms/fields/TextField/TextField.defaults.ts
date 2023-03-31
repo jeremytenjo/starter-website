5f4f9201df261e014b9ed10574e9c1b05ba74764
Jeremy Tenjo <tenjojeremy@gmail.com>
2023-03-31 10:33:50 -0400

    TextFieldProps

--- src/lib/components/useweb/forms/fields/TextField/TextField.defaults.ts
+++ src/lib/components/useweb/forms/fields/TextField/TextField.defaults.ts
@@ -1,8 +1,19 @@
 // https://mui.com/customization/theme-components/#global-style-overrides
-// import in src/theme/mui/MuiProvider.jsx
+// import in src/theme/UiTheme/UiTheme.jsx
 import { type ComponentDefaultsProps } from '@useweb/ui-theme'
 import { type TextFieldProps } from '@useweb/ui/TextField'
+import colors from '../../../../../../theme/tokens/colors'
 
-const defaults: ComponentDefaultsProps<TextFieldProps> = {}
+const defaults: ComponentDefaultsProps<TextFieldProps> = {
+  styleOverrides: {
+    root: {
+      '& .useweb_TextField_label': {
+        color: colors.grey.dark,
+        fontWeight: 700,
+        fontSize: '14px',
+      },
+    },
+  },
+}
 
 export default defaults

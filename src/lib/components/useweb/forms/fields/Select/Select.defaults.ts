7953445e8beec45b1dce4a7de109803446e5599c
Jeremy Tenjo <tenjojeremy@gmail.com>
2023-04-26 10:01:53 -0400

    a

--- src/lib/components/useweb/forms/fields/Select/Select.defaults.ts
+++ src/lib/components/useweb/forms/fields/Select/Select.defaults.ts
@@ -1,8 +1,27 @@
 // https://mui.com/customization/theme-components/#global-style-overrides
-// import in src/theme/mui/MuiProvider.jsx
+// import in src/theme/UiTheme/UiTheme.jsx
 import { type ComponentDefaultsProps } from '@useweb/ui-theme'
 import { type SelectProps } from '@useweb/ui/Select'
+import colors from '../../../../../../theme/tokens/colors'
 
-const defaults: ComponentDefaultsProps<SelectProps> = {}
+const defaults: ComponentDefaultsProps<SelectProps> = {
+  styleOverrides: {
+    root: {
+      borderRadius: '6px',
+      borderColor: colors.grey.med,
+      borderWidth: '1px',
+      borderStyle: 'solid',
+      backgroundColor: colors.white.main,
+      '& .MuiSelect-select': {
+        padding: '10px',
+        paddingLeft: '15px',
+        paddingRight: '15px',
+      },
+      '& fieldset': {
+        border: 'none',
+      },
+    },
+  },
+}
 
 export default defaults

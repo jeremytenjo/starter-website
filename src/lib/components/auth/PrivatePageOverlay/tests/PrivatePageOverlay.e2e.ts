c2815c0b83b84c8835d04ebb3c8be1449af8969a
Jeremy Tenjo <tenjojeremy@gmail.com>
2023-05-16 10:44:11 -0400

    cl

--- src/lib/components/auth/PrivatePageOverlay/tests/PrivatePageOverlay.e2e.ts
+++
@@ -1,7 +0,0 @@
-// import { test } from '@playwright/test'
-
-// import PrivatePageOverlay from './PrivatePageOverlay.test.js'
-
-// test('PrivatePageOverlay works in private pages', async ({ page }) => {
-//   await PrivatePageOverlay({ page })
-// })
--- src/pagesContent/Home/tests/exampleTest/exampleTest.test.ts
+++
@@ -1,9 +0,0 @@
-// https://playwright.dev/docs/selectors
-import { expect } from '@playwright/test'
-
-export default async function exampleTest({ page }) {
-  await page.goto('/')
-
-  const header = await page.innerText('p')
-  expect(header).toBe('We find the best products for you')
-}

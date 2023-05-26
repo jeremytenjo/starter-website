520384f617c5fc76588f066a3db3624c8c1b60fd
Jeremy Tenjo <tenjojeremy@gmail.com>
2023-05-25 21:49:26 -0400

    uu

--- src/lib/components/useweb/forms/fields/TextField/TextField.defaults.ts
+++ src/lib/components/useweb/forms/fields/TextField/TextField.defaults.ts
@@ -4,7 +4,7 @@ import { type ComponentDefaultsProps } from '@useweb/ui-theme'
 import { type TextFieldProps } from '@useweb/ui/TextField'
 import colors from '../../../../../../theme/tokens/colors'
 
-const defaults: ComponentDefaultsProps<TextFieldProps> = {
+const defaults: ComponentDefaultsProps<TextFieldProps<any>> = {
   styleOverrides: {
     root: {
       '& .useweb_TextField_label': {

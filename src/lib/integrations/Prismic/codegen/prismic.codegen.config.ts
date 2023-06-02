bb55f991c479624efb89fb8536f932be703e2507
Jeremy Tenjo <tenjojeremy@gmail.com>
2023-06-02 13:19:06 -0400

    moved slices

--- src/lib/integrations/Prismic/codegen/prismic.codegen.config.ts
+++ src/lib/integrations/Prismic/codegen/prismic.codegen.config.ts
@@ -2,7 +2,10 @@ import type { Config } from 'prismic-ts-codegen'
 
 const config: Config = {
   output: './src/lib/integrations/Prismic/codegen/prismic.codegen.types.ts',
-  models: ['./customtypes/**/index.json', './slices/**/model.json'],
+  models: [
+    './customtypes/**/index.json',
+    './src/lib/integrations/Prismic/slices/**/model.json',
+  ],
 }
 
 export default config
--- src/pages/slice-simulator.tsx
+++ src/pages/slice-simulator.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { SliceSimulator } from '@prismicio/slice-simulator-react'
 import { SliceZone } from '@prismicio/react'
 
-import { components } from '../../slices'
-import state from '../../.slicemachine/libraries-state.json' assert { type: 'json' }
+import { components } from '../lib/integrations/Prismic/slices'
+// import state from '../../.slicemachine/libraries-state.json' assert { type: 'json' }
 
 const SliceSimulatorPage = () => {
   return (
     <SliceSimulator
       sliceZone={({ slices }) => <SliceZone slices={slices} components={components} />}
-      state={state}
+      // state={state}
     />
   )
 }

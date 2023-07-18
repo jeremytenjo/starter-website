e2e2c236504c6413548ddc957771d2b1f102cd42
Jeremy Tenjo <tenjojeremy@gmail.com>
2023-07-18 13:48:18 -0400

    uu

--- devtools/codeGenerator/packageTemplates/templates/firebaseFunction.cjs
+++ devtools/codeGenerator/packageTemplates/templates/firebaseFunction.cjs
@@ -4,28 +4,70 @@ const files = [
   {
     path: ({ name }) => `${name}.ts`,
     template: ({ name, helpers }) => {
-      const pascalCase = helpers.changeCase.pascalCase(name);
-      const propsName = `${pascalCase}Props`;
+      const pascalCase = helpers.changeCase.pascalCase(name)
+      const propsName = `${pascalCase}Props`
+      const returnName = `${pascalCase}Return`
 
       return `export type ${propsName} = {
         name: string
       }
+
+      export type ${returnName} = {
+        hello: string
+      }
       
       export default async function ${name}(
         props: ${propsName},
-      ) {
+      ): Promise<${returnName}> {
         return {
           hello: props.name,
         }
-      }`;
+      }`
+    },
+  },
+  // Client Function - to use in the client (React, js, etc)
+  {
+    path: ({ name }) => `${name}.client.ts`,
+    template: ({ name, helpers }) => {
+      const pascalCase = helpers.changeCase.pascalCase(name)
+      const propsName = `${pascalCase}Props`
+      const returnName = `${pascalCase}Return`
+
+      return `import { googleCloudFunction } from '@useweb/firebase/useFirebaseFunction'
+      import firebaseConfig from '@/src/lib/integrations/Google/Firebase/firebase.config'
+
+      import type {
+        ${propsName},
+        ${returnName},
+      } from './${name}'
+      
+      export default async function ${name}Client(props: ${propsName}) {
+        const res = await googleCloudFunction<
+          ${propsName},
+          {
+            data: ${returnName}
+          }
+        >({
+          name: '${name}',
+          firebase: {
+            firebaseConfig: firebaseConfig,
+            envIsDev: process.env.NODE_ENV === 'development',
+          },
+          options: {
+            payload: props,
+          },
+        })
+      
+        return res
+      }`
     },
   },
   // Firebase Index
   {
     path: ({ name }) => `${name}.firebase.ts`,
     template: ({ name, helpers }) => {
-      const pascalCase = helpers.changeCase.pascalCase(name);
-      const propsName = `${pascalCase}Props`;
+      const pascalCase = helpers.changeCase.pascalCase(name)
+      const propsName = `${pascalCase}Props`
 
       return `
       import ${name}, {
@@ -42,15 +84,15 @@ const files = [
         const res = await ${name}(payload)
       
         return res
-      }`;
+      }`
     },
   },
   // Story
   {
     path: ({ name }) => `stories/${name}.stories.tsx`,
     template: ({ name, helpers }) => {
-      const pascalCase = helpers.changeCase.pascalCase(name);
-      const propsName = `${pascalCase}Props`;
+      const pascalCase = helpers.changeCase.pascalCase(name)
+      const propsName = `${pascalCase}Props`
 
       return `
       import React from 'react'
@@ -78,17 +120,17 @@ export const Default = {
   render: (args: ArgsProps) => {
     return <FirebaseFunctionTester {...args} />
   },
-}`;
+}`
     },
   },
-];
+]
 
 const template = {
   type: 'Firebase Function',
   files,
-};
+}
 
 module.exports = {
   files,
   template,
-};
+}

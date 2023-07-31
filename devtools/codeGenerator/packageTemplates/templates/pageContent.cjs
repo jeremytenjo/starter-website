7a8e47c7bf060f1de813bb29eedda4fa0342d31c
Jeremy Tenjo <tenjojeremy@gmail.com>
2023-07-31 13:08:36 -0400

    uu

--- devtools/codeGenerator/packageTemplates/templates/pageContent.cjs
+++ devtools/codeGenerator/packageTemplates/templates/pageContent.cjs
@@ -32,6 +32,47 @@ export default function ${pascalName}Content() {
 `
     },
   },
+  {
+    parentFolderName: ({ name, helpers }) => {
+      const pascalName = helpers.changeCase.pascalCase(name).split(' ').join('')
+      return pascalName
+    },
+    path: ({ name, helpers }) => {
+      const pascalName = helpers.changeCase.pascalCase(name).split(' ').join('')
+      const componentName = `${pascalName}PageLayout`
+
+      return `${componentName}/${componentName}.tsx`
+    },
+    template: ({ name, helpers }) => {
+      const pascalName = helpers.changeCase.pascalCase(name).replaceAll(' ', '')
+      const componentName = `${pascalName}PageLayout`
+
+      return `import React from 'react'
+      import Box from '@useweb/ui/Box'
+      import Text from '@useweb/ui/Text'
+      
+      export type ${componentName}Props = { children: any }
+      
+      export default function ${componentName}(props: ${componentName}Props) {
+        return (
+          <Wrapper>
+            <Text text={'${componentName}'} tag='p' sx={{}} />
+            {props.children}
+          </Wrapper>
+        )
+      }
+      
+      const Wrapper = ({ children }) => {
+        return (
+          <Box data-id='${componentName}' sx={{}}>
+            {children}
+          </Box>
+        )
+      }
+      
+`
+    },
+  },
 ]
 
 const template = {

49788df3fd842e36930ab9c39f7d4d792d5c7124
Jeremy Tenjo <tenjojeremy@gmail.com>
2023-11-02 01:04:25 -0400

    Update data-id attribute in pageContent template

--- devtools/codeGenerator/packageTemplates/templates/pageContent.cjs
+++ devtools/codeGenerator/packageTemplates/templates/pageContent.cjs
@@ -24,7 +24,7 @@ const files = [
 export default function ${componentName}() {
   return (
     <${componentName}Layout>
-      <Box data-id='${name}' sx={{}}>
+      <Box data-id='${componentName}' sx={{}}>
       ${slots?.childContainers?.importedComponents || pascalName}
       </Box>
     </${componentName}Layout>

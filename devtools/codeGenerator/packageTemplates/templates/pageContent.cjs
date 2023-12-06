cbb3dc051dbc9997c51b91474d6b1c9fc8d31391
Jeremy Tenjo <tenjojeremy@gmail.com>
2023-12-06 12:07:42 -0500

    Update package version to 9.15.0 and add containers/readme.md template

--- devtools/codeGenerator/packageTemplates/templates/pageContent.cjs
+++ devtools/codeGenerator/packageTemplates/templates/pageContent.cjs
@@ -63,9 +63,25 @@ export default function ${componentName}() {
           </Box>
         )
       }
-      
-      
-`
+      `
+    },
+  },
+  {
+    parentFolderName: ({ name, helpers }) => {
+      const pascalName = helpers.changeCase.pascalCase(name).split(' ').join('')
+      return pascalName
+    },
+    path: () => {
+      return `containers/readme.md`
+    },
+    template: ({ name, helpers }) => {
+      const pascalName = helpers.changeCase.pascalCase(name).replaceAll(' ', '')
+      const componentName = `${pascalName}PageLayout`
+
+      return `## ${componentName} Containers
+
+        Containers are components that are used to pass data to other components. They are used to separate data fetching from the components that use the data.
+      `
     },
   },
 ]
